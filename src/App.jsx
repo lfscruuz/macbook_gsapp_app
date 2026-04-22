46120e246195494dfa17236f7c52fdd10953b5d0
lfscruz <lfscruz@outlook.com>
2026-04-22 02:23:23 -0300

    basic product view with cube

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero"
 import Navbar from "./components/NavBar"
+import ProductViewer from "./components/ProductViewer"
 
 const App = () => {
   return (
     <main>
       <Navbar/>
       <Hero/>
+      <ProductViewer/>
     </main>
   )
 }
